dc9a00b70049795fefe21a2f346df22f27540c51
Malay Dewari <dewarimalay@gmail.com>
2025-08-05 00:07:18 +0530

    chapter 2 (conditional statement and loops)

---
+++ Loops.py
@@ -0,0 +1,11 @@
+fruits = ["apple", "banana", "cherry", "pineapple"]
+for fruit in fruits:
+    print(fruit)
+
+for i in range(5):
+    print(i)
+
+count = 0
+while count < 4:
+    print("Count is ", count)
+    count = count + 1
---
+++ conditionalStmt.py
@@ -0,0 +1,9 @@
+number_as_str = input("Enter a number: ")
+number = int(number_as_str)
+
+if number>0:
+    print("Positive number")
+elif number == 0:
+    print("Number is zero")
+else:
+    print("Negative number")
